ca04eae8e28105dfabde0c5a8d3a4f8b4a7809a5
Sergey <doroshenkoeng@gmail.com>
2019-09-13 13:20:19 +0300

    Initial Commit

---
+++ GitTutorial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitTutorial
+//
+//  Created by Сергей Дорошенко on 13/09/2019.
+//  Copyright © 2019 Сергей Дорошенко. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
